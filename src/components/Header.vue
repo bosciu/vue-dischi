4959493a88b1971ea6a97cced3ebbf69bac6759f
bosciu <bosciu@icloud.com>
2021-06-09 15:04:03 +0200

    Completamento Header

---
+++ src/components/Header.vue
@@ -0,0 +1,23 @@
+<template>
+	<header>
+		<img src="../assets/logo.png" alt="Spotify" />
+	</header>
+</template>
+
+<script>
+export default {
+	name: "Header"
+};
+</script>
+
+<style lang="scss" scoped>
+@import "../style/variables";
+header {
+	height: 100px;
+	padding: 15px;
+	background-color: $mainGrey;
+	img {
+		height: 100%;
+	}
+}
+</style>
